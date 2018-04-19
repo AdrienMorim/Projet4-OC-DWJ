1f89d1d9d949ddda35442230cd7cc4176d33e5d1
Morim Adrien <adrien.986@gmail.com>
2018-04-19 13:54:34 +0200

    Creation de la V1-Procedural + dossier assets avec css + index.php avec connexion a la db

---
+++ V1-Procedural/Index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <meta name="description" content="Roman 'Billet simple pour l'Alaska', nouveau Roman de Jean Forteroche"/>
+        <meta name="keywords" content="Jean Forteroche, Blog, Roman, Billet simple pour l'Alaska"/>
+        <meta name="author" content="Jean Forteroche">
+        <link rel="stylesheet" type="text/css" href="assets/css/style.css"/>
+        <link rel="icon" type="image/ico" sizes="500X500" href="images/ico/favicon.png" />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
+        <link href="https://fonts.googleapis.com/css?family=Audiowide|Orbitron:400,500" rel="stylesheet">
+        <script src="https://code.jquery.com/jquery-3.2.1.js" integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE=" crossorigin="anonymous"></script>
+        <title>Billet simple Pour l'Alaska</title>
+    </head>
+
+    <body>
+        <h1>Billet simple pour l'Alaska</h1>
+        <h2>Nouveau Roman - Jean Forteroche</h2>
+
+    <?php
+    //Connexion DataBase
+    try
+    {
+        $db = new PDO('mysql:host=localhost;dbname=projet4;charset=utf-8', 'root', 'root');
+    }
+    catch (Exception $e)
+    {
+        die('Erreur : ' .$e->getMessage());
+    }
+
+
+    ?>
+    </body>
+</html>
+
+
