998a6a2e8d6164773ca300057cb5fcf774b33d15
Morim Adrien <adrien.986@gmail.com>
2018-04-28 20:39:38 +0200

    creation de la requete qui verifie les donnees de connexion

--- V1-Procedural/log.php
+++ V1-Procedural/log.php
@@ -1,7 +1,45 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: morimadrien
- * Date: 28/04/2018
- * Time: 19:52
- */
+
+    // Connexion à la base de données
+    try
+    {
+        $host = 'localhost';
+        $database = 'projet4';
+        $username = 'root';
+        $password = 'root';
+        $db = new PDO('mysql:host=' . $host . ';dbname=' . $database . ';charset=utf8', $username, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    }
+    catch(Exception $e)
+    {
+        echo 'Veuillez patienter.. La base de données n\'est pas encore disponible. <br />';
+        die('Erreur : ' . $e->getMessage());
+    }
+
+    $req = $db->prepare('SELECT id, pass FROM users WHERE pseudo= :pseudo');
+    $req->execute(array(
+        'pseudo'=> $_POST['pseudo']
+    ));
+    $data = $req->fetch();
+
+    $proper_pass = password_verify($_POST['password'], $data['pass']);
+
+    if(!$data)
+    {
+        echo 'Mauvais identifiant ou mot de passe';
+    }
+    else
+    {
+        if($proper_pass)
+        {
+            session_start();
+            $_SESSION['id'] = $data['id'];
+            $_SESSION['pseudo'] = $data['pseudo'];
+
+            // redirection
+            header( 'Location: index.php');
+        }
+        else
+        {
+            echo 'Mauvais identifiant ou mot de passe';
+        }
+    }
