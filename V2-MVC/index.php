df16971f035a6e845fcb6844cea1aedf9f73a1a9
Morim Adrien <adrien.986@gmail.com>
2018-04-30 10:19:07 +0200

    ajout de throw new Exception() pour la gestion d'exception avec try/catch + redirection vers la vue errorView.php

--- V2-MVC/index.php
+++ V2-MVC/index.php
@@ -2,29 +2,60 @@
 
 require('controller/frontend.php');
 
-if (isset($_GET['action']))
-{
-    if($_GET['action'] == '')
+// Gestion des exception
+try{
+    if (isset($_GET['action']))
+    {
+        // Index
+        if($_GET['action'] == NULL)
+        {
+            lastOne();
+        }
+        // Liste des chapitres
+        elseif ($_GET['action'] == 'listChapters')
+        {
+            listChapters();
+        }
+        // Affiche le chapitre avec ses commentaires
+        elseif ($_GET['action'] == 'chapter')
+        {
+            if (isset($_GET['id_chapter']) && $_GET['id_chapter'] > 0)
+            {
+                chapter();
+            }
+            else
+            {
+                throw new Exception('Aucun identifiant de chapitre envoyé !');
+            }
+        }
+        // Ajoute un commentaire dans le chapitre selectionné
+        elseif ($_GET['action'] == 'addComment')
+        {
+            if (isset($_GET['id_chapter']) && $_GET['id_chapter'] > 0)
+            {
+                if (!empty($_POST['author']) && !empty($_POST['comment']))
+                {
+                    addComment($_GET['id_chapter'], $_POST['author'], $_POST['comment']);
+                }
+                else
+                {
+                    throw new Exception('Tous les champs doivent être remplis !');
+                }
+            }
+            else
+            {
+                throw new Exception('Aucun identifiant de chapitre envoyé !');
+            }
+        }
+    }
+// Retourne à l'index.
+    else
     {
         lastOne();
     }
-    elseif ($_GET['action'] == 'listChapters')
-    {
-        listChapters();
-    }
-    elseif ($_GET['action'] == 'chapter')
-    {
-        if (isset($_GET['chapter']) && $_GET['chapter'] > 0)
-        {
-            chapter();
-        }
-        else
-        {
-            echo 'Erreur : aucun identifiant de chapitre envoyé !';
-        }
-    }
 }
-else
+catch (Exception $e)
 {
-    lastOne();
+    $errorMessage = $e->getMessage();
+    require('view/frontend/errorView.php');
 }
