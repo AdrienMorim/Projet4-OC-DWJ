decc59735b3740fdc0d49568693e9976dcca22d3
Morim Adrien <adrien.986@gmail.com>
2018-04-30 10:13:42 +0200

    ajout de throw new Exception() pour la gestion des erreurs

--- V2-MVC/controller/frontend.php
+++ V2-MVC/controller/frontend.php
@@ -17,7 +17,19 @@ function listChapters()
 
 function chapter()
 {
-    $chapter = getChapter($_GET['chapter']);
-    $comments = getComments($_GET['chapter']);
+    $chapter = getChapter($_GET['id_chapter']);
+    $comments = getComments($_GET['id_chapter']);
     require('view/frontend/chapterView.php');
+}
+
+function addComment($id_chapter, $author, $comment)
+{
+    $postComment = postComment($id_chapter, $author, $comment);
+    if($postComment === false)
+    {
+        throw new Exception('Impossible d\'ajouter le commentaire');
+    }
+    else{
+        header('Location: ../V2-MVC/index.php?action=chapter&id_chapter=' . $id_chapter);
+    }
 }
