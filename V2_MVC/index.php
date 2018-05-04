9f667f96863b3fc9e9f13656e608381f0bbd481d
Morim Adrien <adrien.986@gmail.com>
2018-05-04 15:37:38 +0200

    ajout des conditions pour la page login, log et register

--- V2_MVC/index.php
+++ V2_MVC/index.php
@@ -7,9 +7,10 @@ try{
     if (isset($_GET['action']))
     {
         // Index
-        if($_GET['action'] == NULL)
+        if($_GET['action'] == '')
         {
             lastOne();
+            //require('view/frontend/loginView.php');
         }
         // Liste des chapitres
         elseif ($_GET['action'] == 'listChapters')
@@ -45,6 +46,56 @@ try{
             else
             {
                 throw new Exception('Aucun identifiant de chapitre envoyé !');
+            }
+        }
+        // Page de connexion
+        elseif($_GET['action'] == 'login')
+        {
+            login();
+            //require('view/frontend/loginView.php');
+        }
+        // Connexion
+        elseif ($_GET['action'] == 'log')
+        {
+            if (!empty($_POST['pseudo']) && !empty($_POST['pass']))
+            {
+                logUser($_POST['pseudo'], $_POST['pass']);
+            }
+            else
+            {
+                throw new Exception('Tous les champs doivent être remplis !');
+            }
+        }
+        // Inscription
+        elseif ($_GET['action'] == 'register'){
+            if (!empty($_POST['pseudo']) && !empty($_POST['password']) && !empty($_POST['password_confirm']) && !empty($_POST['email']))
+            {
+                // Sécurité
+                $pseudo = htmlspecialchars($_POST['pseudo']);
+                $email = htmlspecialchars($_POST['email']);
+                // Hachage du mot de passe
+                $password_hache = password_hash($_POST['password'], PASSWORD_DEFAULT);
+                // On vérifie la Regex pour l'adresse email
+                if (preg_match("#^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$#", $_POST['email']))
+                {
+                    // On vérifie que les 2 mots de passe sont identiques.
+                    if ($_POST['password'] == $_POST['password_confirm'])
+                    {
+                        registerUser($pseudo, $password_hache, $email);
+                    }
+                    else
+                    {
+                        throw new Exception('Les 2 mots de passe ne sont pas identiques, recommencez !');
+                    }
+                }
+                else
+                {
+                    throw new Exception('L\'adresse email ' . $email . ' n\'est pas valide, recommencez !');
+                }
+            }
+            else
+            {
+                throw new Exception('Tous les champs doivent être remplis !');
             }
         }
     }
