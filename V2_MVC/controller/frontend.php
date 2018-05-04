b360d591915c3cf8de759810c482c707f9c020ca
Morim Adrien <adrien.986@gmail.com>
2018-05-04 15:43:45 +0200

    ajout des fonction login, logUser et registerUser

--- V2_MVC/controller/frontend.php
+++ V2_MVC/controller/frontend.php
@@ -3,9 +3,11 @@
 //require('model/frontend.php');
 require_once('model/ChapterManager.php');
 require_once('model/CommentManager.php');
+require_once('model/UsersManager.php');
 
 use V2_MVC\Model\ChapterManager;
 use V2_MVC\Model\CommentManager;
+use V2_MVC\Model\UsersManager;
 
 function lastOne()
 {
@@ -45,5 +47,51 @@ function addComment($id_chapter, $author, $comment)
     }
     else{
         header('Location: ../V2_MVC/index.php?action=chapter&id_chapter=' . $id_chapter);
+    }
+}
+
+function login()
+{
+    require('view/frontend/loginView.php');
+}
+
+function logUser($pseudo, $pass)
+{
+    $userManager = new UsersManager();
+
+    $user = $userManager->getUser($pseudo);
+    $proper_pass = password_verify($_POST['pass'], $user['pass']);
+
+    if(!$user)
+    {
+        throw new Exception('Wrong username or password');
+    }
+    else{
+        if($proper_pass)
+        {
+            session_start();
+            $_SESSION['id'] = $user['id'];
+            $_SESSION['pseudo'] = $user['pseudo'];
+
+            header('Location: ../V2_MVC/index.php');
+        }
+        else
+        {
+            throw new Exception('Wrong username or password');
+        }
+    }
+}
+
+function registerUser($pseudo, $password_hache, $email){
+
+    $userManager = new UsersManager();
+    $registerUser = $userManager->createUser($pseudo, $password_hache, $email);
+    if($registerUser === false)
+    {
+        throw new Exception('Impossible d\'inscrire le nouvel utilisateur');
+    }
+    else
+    {
+        header('Location: ../V2_MVC/index.php');
     }
 }
