244a11e1d401c0b381fe57473be9992a74c78a01
Morim Adrien <adrien.986@gmail.com>
2018-05-04 17:06:55 +0200

    modification du lien vers la page A propos

--- V2_MVC/view/frontend/loginView.php
+++ V2_MVC/view/frontend/loginView.php
@@ -7,7 +7,7 @@
             <a href="../V2_MVC/index.php">Accueil</a>
         </li>
         <li>
-            <a href="about.php">À propos</a>
+            <a href="../V2_MVC/index.php?action=about">À propos</a>
         </li>
         <li>
             <a href="../V2_MVC/index.php?action=listChapters">Chapitres</a>
