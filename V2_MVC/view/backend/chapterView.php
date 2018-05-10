b3b23f780c90c6b980338ad473ef1850290740d4
Morim Adrien <adrien.986@gmail.com>
2018-05-10 16:02:23 +0200

    ajout de la page chapterView dans backend

--- V2_MVC/view/backend/chapterView.php
+++ V2_MVC/view/backend/chapterView.php
@@ -1,1 +1,86 @@
+<?php $title = 'Admin - Chapitre ' . htmlspecialchars($chapter['id']) . ' - Billet simple pour l\'Alaska'; ?>
+
+<?php ob_start(); ?>
+
+    <ul> <!-- avec emmet: ul>li*5>a +[TAB]-->
+        <li>
+            <a href="../V2_MVC/index.php?action=dashbord">Tableau de bord</a>
+        </li>
+        <li>
+            <a href="../V2_MVC/index.php?action=about">À propos</a>
+        </li>
+        <li>
+            <a href="../V2_MVC/index.php?action=createChapter">Créer un chapitre</a>
+        </li>
+        <li>
+            <a href="../V2_MVC/index.php?action=updateChapter">Mise à jour d'un chapitre</a>
+        </li>
+        <li>
+            <a href="../V2_MVC/index.php?action=deleteChapter">Supprimer un chapitre</a>
+        </li>
+        <li>
+            <a href="..V2_MVC/index.php?action=adminComment">Administration des commentaires</a>
+        </li>
+    </ul>
+
+<?php $admin_menu = ob_get_clean(); ?>
+
+<?php ob_start(); ?>
+
+    <h1>Admin - Billet simple pour l'Alaska</h1>
+    <h2>Chapitre <?= htmlspecialchars($chapter['id']); ?></h2>
+
+    <div class="news">
+        <h3>
+            <?= htmlspecialchars($chapter['title']); ?>
+            <em>le <?= $chapter['creation_date_fr']; ?></em>
+        </h3>
+
+        <p>
+            <?= nl2br(htmlspecialchars($chapter['content'])); ?> <br/>
+        </p>
+    </div>
+
+    <h3>Commentaires</h3>
+
 <?php
+
+    while ($comment = $comments->fetch())
+    {
+        ?>
+        <p><strong><?= htmlspecialchars($comment['author']) ?></strong> le <?= $comment['comment_date_fr'] ?></p>
+        <p><?= nl2br(htmlspecialchars($comment['comment'])) ?></p>
+        <em><a href="../V2_MVC/index.php?action=report&amp;id_chapter=<?= $chapter['id'];?>&amp;id=<?= $comment['id'];?>">Signaler le commentaire</a></em>
+        <?php
+    }
+
+    $comments->closeCursor();
+?>
+
+    <div class="news">
+        <form action="../V2_MVC/index.php?action=addComment&amp;id_chapter=<?= $_GET['id_chapter'];?>" method="POST">
+            <h3> Ajouter votre commentaire:</h3>
+            <p>
+                <label for="author">Auteur
+                    <input type="text" name="author" id="author" placeholder="Indiquez ici votre nom" value="<?php
+                    if (isset($_SESSION['author'])){
+                        echo htmlspecialchars($_SESSION['author']);
+                    }?>"
+                    />
+                </label>
+            </p>
+            <p>
+                <label for="comment">Commentaire
+                    <textarea name="comment" id="comment" placeholder="Indiquez ici votre commentaire"></textarea>
+                </label>
+            </p>
+            <button>
+                <input type="submit" value="Envoyer votre commentaire"/>
+            </button>
+        </form>
+    </div>
+
+<?php $content = ob_get_clean(); ?>
+
+<?php require('template.php'); ?>
+
