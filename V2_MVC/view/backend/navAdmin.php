752ddbd9df4beb3fa500cb0e00f46c544f1c328d
Morim Adrien <adrien.986@gmail.com>
2018-05-12 13:39:22 +0200

    ajout du navAdmin pour l'inclusion dans les vues

--- V2_MVC/view/backend/navAdmin.php
+++ V2_MVC/view/backend/navAdmin.php
@@ -1,7 +1,32 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: morimadrien
- * Date: 12/05/2018
- * Time: 12:01
- */
+<ul> <!-- avec emmet: ul>li*5>a +[TAB]-->
+    <li>
+        <a href="../V2_MVC/index.php?action=dashbord">Tableau de bord</a>
+    </li>
+    <li>
+        <a href="../V2_MVC/index.php?action=adminListChapters">Administration des chapitres</a>
+    </li>
+    <li>
+        <a href="../V2_MVC/index.php?action=adminListComments">Administration des commentaires</a>
+    </li>
+    <li>
+        <a href="../V2_MVC/index.php?action=logout">Déconnexion</a>
+    </li>
+</ul>
+
+<ul> <!-- avec emmet: ul>li*5>a +[TAB]-->
+    <li>
+        <a href="../V2_MVC/index.php">Accueil</a>
+    </li>
+    <li>
+        <a href="../V2_MVC/index.php?action=about">À propos</a>
+    </li>
+    <li>
+        <a href="../V2_MVC/index.php?action=listChapters">Chapitres</a>
+    </li>
+    <li>
+        <a href="../V2_MVC/index.php?action=login">Inscription/Connexion</a>
+    </li>
+    <li>
+        <a href="../V2_MVC/index.php?action=logout">Déconnexion</a>
+    </li>
+</ul>
