6b33774e378a674e3752a16acabe368388e3d570
Morim Adrien <adrien.986@gmail.com>
2018-05-12 13:40:04 +0200

    ajout du nav pour l'inclusion dans les vues

--- V2_MVC/view/frontend/nav.php
+++ V2_MVC/view/frontend/nav.php
@@ -1,7 +1,17 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: morimadrien
- * Date: 12/05/2018
- * Time: 11:51
- */
+<ul> <!-- avec emmet: ul>li*5>a +[TAB]-->
+    <li>
+        <a href="../V2_MVC/index.php">Accueil</a>
+    </li>
+    <li>
+        <a href="../V2_MVC/index.php?action=about">À propos</a>
+    </li>
+    <li>
+        <a href="../V2_MVC/index.php?action=listChapters">Chapitres</a>
+    </li>
+    <li>
+        <a href="../V2_MVC/index.php?action=login">Inscription/Connexion</a>
+    </li>
+    <li>
+        <a href="../V2_MVC/index.php?action=logout">Déconnexion</a>
+    </li>
+</ul>
