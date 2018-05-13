03b557b56c539620bf60c99eac0836c02f8488c9
Morim Adrien <adrien.986@gmail.com>
2018-05-13 14:46:09 +0200

    petite maj du template

--- V2_MVC/view/template.php
+++ V2_MVC/view/template.php
@@ -1,7 +1,45 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: morimadrien
- * Date: 13/05/2018
- * Time: 09:52
- */
+
+if (isset($_SESSION['id']) AND isset($_SESSION['pseudo']))
+{
+    echo 'Bonjour ' . $_SESSION['pseudo'] . ', tu es admin de level: ' . $_SESSION['id_group'];
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="'Billet simple pour l'Alaska', est le nouveau Roman de Jean Forteroche. Écrivain, auteur de plusieurs bestsellers, décide d'écrire son nouveau roman avec plusieurs épisodes sous forme de blog."/>
+        <meta name="keywords" content="Jean Forteroche, Blog, Roman, Billet simple pour l'Alaska"/>
+        <meta name="author" content="Jean Forteroche">
+        <!-- Link Style - public -->
+        <link rel="stylesheet" type="text/css" href="../V2_MVC/public/css/style.css"/>
+        <link rel="icon" type="image/ico" sizes="500X500" href="../V2_MVC/public/images/ico/favicon.png" />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/css?family=Audiowide|Orbitron:400,500" rel="stylesheet">
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+        <title> <?= $title ?> </title>
+    </head>
+
+    <body>
+        <div class="container fluid">
+            <header>
+                <nav> <?= $menu ?> </nav>
+                <?= $header ?>
+            </header>
+            <div class="container">
+                <?= $content ?>
+            </div>
+            <footer>
+                <!-- <?= $footer ?> -->
+            </footer>
+        </div>
+    </body>
+</html>
