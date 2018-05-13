30d1dfa7408fc687f4ddda0dc42b2d39b72a3664
Morim Adrien <adrien.986@gmail.com>
2018-05-13 14:44:39 +0200

    creation d'un seul fichier nav.php avec des conditions pour les differents utilisateurs

--- V2_MVC/view/nav.php
+++ V2_MVC/view/nav.php
@@ -1,7 +1,79 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: morimadrien
- * Date: 13/05/2018
- * Time: 09:52
- */
+
+if(isset($_SESSION['id']) && $_SESSION['id_group'] == 1)
+{
+    ?>
+    <ul>
+        <li>
+            <a href="../V2_MVC/index.php?action=dashbord">Tableau de bord</a>
+        </li>
+        <li>
+            <a href="../V2_MVC/index.php?action=adminListChapters">Administration des chapitres</a>
+        </li>
+        <li>
+            <a href="../V2_MVC/index.php?action=adminListComments">Administration des commentaires</a>
+        </li>
+        <li>
+            <a href="../V2_MVC/index.php?action=#">Administration des membres</a>
+        </li>
+        <li>
+            <a href="../V2_MVC/index.php">Accueil Visiteur</a>
+        </li>
+        <li>
+            <a href="../V2_MVC/index.php?action=about">À propos</a>
+        </li>
+        <li>
+            <a href="../V2_MVC/index.php?action=listChapters">Chapitres</a>
+        </li>
+        <li>
+            <a href="../V2_MVC/index.php?action=adminParametre">Paramètres</a>
+        </li>
+    </ul>
+    <ul>
+        <li>
+            <a href="../V2_MVC/index.php?action=logout">Déconnexion</a>
+        </li>
+    </ul>
+<?php
+}
+elseif (isset($_SESSION['id']) && $_SESSION['id_group'] == 2)
+{
+    ?>
+    <ul>
+        <li>
+            <a href="../V2_MVC/index.php">Accueil</a>
+        </li>
+        <li>
+            <a href="../V2_MVC/index.php?action=about">À propos</a>
+        </li>
+        <li>
+            <a href="../V2_MVC/index.php?action=listChapters">Chapitres</a>
+        </li>
+        <li>
+            <a href="../V2_MVC/index.php?action=parametres">Paramètres</a>
+        </li>
+        <li>
+            <a href="../V2_MVC/index.php?action=logout">Déconnexion</a>
+        </li>
+    </ul>
+<?php
+}
+else
+{
+    ?>
+    <ul>
+        <li>
+            <a href="../V2_MVC/index.php">Accueil</a>
+        </li>
+        <li>
+            <a href="../V2_MVC/index.php?action=about">À propos</a>
+        </li>
+        <li>
+            <a href="../V2_MVC/index.php?action=listChapters">Chapitres</a>
+        </li>
+        <li>
+            <a href="../V2_MVC/index.php?action=login">Inscription/Connexion</a>
+        </li>
+    </ul>
+<?php
+}
