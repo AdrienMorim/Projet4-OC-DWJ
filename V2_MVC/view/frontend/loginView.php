a4b8378c3b4af9826dba4e8712f3043c2c764939
Morim Adrien <adrien.986@gmail.com>
2018-05-18 21:28:12 +0200

    modification des classes + style

--- V2_MVC/view/frontend/loginView.php
+++ V2_MVC/view/frontend/loginView.php
@@ -12,20 +12,23 @@
 <?php ob_start(); ?>
 
     <form action="../V2_MVC/index.php?action=log" method="POST">
-        <h3 class="col-md-offset-5 col-md-4">Connexion</h3>
-        <p>
-            <label for="pseudo">Pseudo
-                <input type="text" name="pseudo" id="pseudo" placeholder="Indiquez ici votre nom d'utilisateur" value=""/>
-            </label>
-        </p>
-        <p>
-            <label for="pass">Mot de passe
-                <input type="password" name="pass" id="pass" placeholder="Indiquez ici votre mot de passe"/>
-            </label>
-        </p>
-        <button>
-            <input type="submit" value="Connexion"/>
-        </button>
+        <div class="col-lg-12">
+            <div class="form-group row">
+                <h3 class="col-lg-offset-5 col-lg-4">Connexion</h3>
+            </div>
+            <div class="form-group row">
+                <label for="pseudo" class="col-md-3">Pseudo :</label>
+                <div class="col-md-9">
+                    <input type="text" name="pseudo" id="pseudo" class="form-control" placeholder="Pseudo"/>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="password" class="col-md-3">Mot de Passe :</label>
+                <div class="col-md-9">
+                    <input type="password" name="pass" id="password" class="form-control" placeholder="Mot de passe"/>
+                </div>
+            </div>
+            <button type="submit" name="connexion"  class="btn btn-primary">Connexion</button>
     </form>
 
     <form action="../V2_MVC/index.php?action=register" method="post">
@@ -36,26 +39,25 @@
             <div class="form-group row">
                 <label for="pseudo" class="col-md-3">Pseudo :</label>
                 <div class="col-md-9">
-                    <input type="text" name="pseudo" id="pseudo" class="form-control" placeholder="Php"
-                           value="<?php if (isset($_COOKIE['pseudo'])) { echo htmlspecialchars($_COOKIE['pseudo']); }?>"/>
+                    <input type="text" name="pseudo" id="pseudo" class="form-control" placeholder="Pseudo"/>
                 </div>
             </div>
             <div class="form-group row">
                 <label for="password" class="col-md-3">Mot de Passe :</label>
                 <div class="col-md-9">
-                    <input type="password" name="password" id="password" class="form-control" placeholder="MySQL4.4"/>
+                    <input type="password" name="pass" id="password" class="form-control" placeholder="Mot de passe"/>
                 </div>
             </div>
             <div class="form-group row">
                 <label for="password_confirm" class="col-md-3">Confirmation du Mot de Passe :</label>
                 <div class="col-md-9">
-                    <input type="password" name="password_confirm" id="password_confirm" class="form-control" placeholder="MySQL4.4"/>
+                    <input type="password" name="password_confirm" id="password_confirm" class="form-control" placeholder="Mot de passe"/>
                 </div>
             </div>
             <div class="form-group row">
                 <label for="email" class="col-md-3">E-mail :</label>
                 <div class="col-md-9">
-                    <input type="email" name="email" id="email" class="form-control" placeholder="mysql@oc.com"/>
+                    <input type="email" name="email" id="email" class="form-control" placeholder="nom.prenom@email.com"/>
                 </div>
             </div>
             <button type="submit" name="inscription" class="btn btn-primary col-md-offset-5 col-md-2">Inscription</button>
