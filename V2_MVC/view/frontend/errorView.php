f2451354a1c7f4e284ce3189ba8939c3ab0e6754
Morim Adrien <adrien.986@gmail.com>
2018-05-18 21:23:39 +0200

    verification de session pour les message d'erreur

--- V2_MVC/view/frontend/errorView.php
+++ V2_MVC/view/frontend/errorView.php
@@ -5,7 +5,7 @@
 <?php ob_start(); ?>
 
     <h1>Billet simple pour l'Alaska</h1>
-    <h2>Attention <?= $_SESSION['pseudo']?></h2>
+    <h2>Attention <?php if(isset($_SESSION['id'])) { echo $_SESSION['pseudo']; } ?></h2>
     <h3><i class="fas fa-exclamation-triangle fa-2x"></i></h3>
 
 <?php $header = ob_get_clean(); ?>
