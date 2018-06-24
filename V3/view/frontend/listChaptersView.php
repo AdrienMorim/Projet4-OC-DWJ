c150cf730064de491ea5f40d2cd0958f44b0b35d
Morim Adrien <adrien.986@gmail.com>
2018-06-24 12:46:28 +0200

    ajout d'une table pour une meilleure visibilité

--- V3/view/frontend/listChaptersView.php
+++ V3/view/frontend/listChaptersView.php
@@ -7,37 +7,54 @@
 
 <?php $header = ob_get_clean(); ?>
 
-<?php ob_start();
+<?php ob_start(); ?>
+<div class="table-responsive">
+    <table class="table table-hover">
+        <thead>
+            <tr class="table-primary">
+                <th scope="col">Titre </th>
+                <th scope="col">Date de création</th>
+                <th scope="col">Aperçu</th>
+                <th scope="col">Éditer</th>
+                <th scope="col">Supprimer</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php while ($data = $chapters->fetch())
+            {
+                ?>
+                <tr>
+                    <th scope="row"><?= htmlspecialchars($data['title']); ?></th>
+                    <td>le <?= $data['creation_date_fr']; ?></td>
+                    <td>
+                        <?= nl2br(htmlspecialchars(substr($data['content'], 0, 150))); ?>...
+                        <?php if(!isset($_SESSION['id_group']) || isset($_SESSION) && $_SESSION['id_group'] == 2) { ?>
+                            <em><a href="../V3/index.php?action=chapter&amp;id_chapter=<?= $data['id']; ?>">Voir la suite <i class="fas fa-arrow-alt-circle-right"></i></a></em>
+                        <?php
+                        }
+                        else { ?>
+                            <em><a href="../V3/index.php?action=chapter&amp;id_chapter=<?= $data['id']; ?>">Voir la suite <i class="fas fa-arrow-alt-circle-right"></i></a></em>
+                    </td>
 
-while ($data = $chapters->fetch())
-{
-    ?>
-    <div class="news">
-        <h3>
-            <?= htmlspecialchars($data['title']); ?>
-            <em>le <?= $data['creation_date_fr']; ?></em>
-        </h3>
+                    <td>
+                        <a href="../V3/index.php?action=adminUpdateChapter&amp;id_chapter=<?= $data['id']; ?>"><i class="fas fa-pen-square"></i></a>
+                    </td>
+                    <td>
+                        <a href="../V3/index.php?action=deleteChapter&amp;id_chapter=<?= $data['id']; ?>"><i class="fas fa-trash-alt"></i></a>
+                    </td>
+                        <?php
+                        }
+                        ?>
 
-        <p>
-            <?= nl2br(htmlspecialchars(substr($data['content'], 0, 150))); ?>...<br/>
-            <?php if(!isset($_SESSION['id_group']) || isset($_SESSION) && $_SESSION['id_group'] == 2) { ?>
-                <em><a href="../V3/index.php?action=chapter&amp;id_chapter=<?= $data['id']; ?>">Voir la suite <i class="fas fa-arrow-alt-circle-right"></i></a></em>
-            <?php
-            }
-            else { ?>
-                <em><a href="../V3/index.php?action=chapter&amp;id_chapter=<?= $data['id']; ?>">Voir la suite <i class="fas fa-arrow-alt-circle-right"></i></a></em>
-                <em><a href="../V3/index.php?action=adminUpdateChapter&amp;id_chapter=<?= $data['id']; ?>">Éditer <i class="fas fa-pen-square"></i></a></em>
-                <em><a href="../V3/index.php?action=deleteChapter&amp;id_chapter=<?= $data['id']; ?>">Supprimer <i class="fas fa-trash-alt"></i></a></em>
-            <?php
-            }
+                </tr>
+                <?php
+            } // fin de la boucle des chapitres
+
+            $chapters->closeCursor();
             ?>
-        </p>
-    </div>
-    <?php
-} // fin de la boucle des chapitres
-
-$chapters->closeCursor();
-?>
+        </tbody>
+    </table>
+</div>
 
 <?php $content = ob_get_clean(); ?>
 
