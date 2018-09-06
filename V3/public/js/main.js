2551cb6f35191651947b05592e8a251e23d037d8
Morim Adrien <adrien.986@gmail.com>
2018-09-06 16:58:37 +0200

    ajout du js pour le menu toggle

---
+++ V3/public/js/main.js
@@ -0,0 +1,12 @@
+/*==========================================================================*\
+ *																			*
+ *      main.js PROJET 4 - Billet Simple pour l'Alaska - ADRIEN MORIM       *
+ *																			*
+\*==========================================================================*/
+
+// main.js permet d'initialisé les  fichiers '.js' une fois que le DOM est chargé.
+// Dès que jQuery est chargé execute cette fonction
+
+$(function() {
+    toggle.initToggle(); // initialise toggle.js
+});
