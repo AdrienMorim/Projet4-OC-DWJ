996bc769d79e65b33d1a7c721d4b5f773be2d187
Morim Adrien <adrien.986@gmail.com>
2019-03-07 13:33:20 +0100

    modification du nom du Controller

---
+++ V3/controller/DashbordController.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: morimadrien
+ * Date: 14/06/2018
+ * Time: 16:34
+ */
+
+namespace V3\Controller;
+
+require_once ('model/ChapterManager.php');
+require_once ('model/CommentManager.php');
+require_once ('model/UserManager.php');
+
+use V3\Model\ChapterManager;
+use V3\Model\CommentManager;
+use V3\Model\UserManager;
+
+class DashbordController
+{
+    private $_chapter;
+    private $_comment;
+    private $_user;
+
+    public function __construct()
+    {
+        $this->_chapter = new ChapterManager();
+        $this->_comment = new CommentManager();
+        $this->_user = new UserManager();
+    }
+
+    public function dashbord()
+    {
+        $chapter = $this->_chapter->getLastChapter();
+        $comment = $this->_comment->getLastComment();
+        $chaptersTotal = $this->_chapter->countChapters();
+        $commentsTotal = $this->_comment->countComments();
+        $commentsReportTotal = $this->_comment->countCommentsReport();
+        $usersTotal = $this->_user->countUsers();
+        require('view/backend/dashbordView.php');
+    }
+}
