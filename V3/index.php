471d78bf222287501e557fe65aded75cf03c64df
Morim Adrien <adrien.986@gmail.com>
2019-03-17 19:01:17 +0100

    apercu des erreurs en commentaires

--- V3/index.php
+++ V3/index.php
@@ -6,8 +6,8 @@ $_SESSION['id_group'];
 
 require('src/controller/Routeur.php');
 
-ini_set( "error_reporting", E_ALL );
-ini_set( "display_errors", 1 );
+//ini_set( "error_reporting", E_ALL );
+//ini_set( "display_errors", 1 );
 
 use Alaska\Src\Controller\Routeur;
 
